2322a9f619773f0e21c8c6c1d6602fe06b4f394d
Nicholas Handaja <nhandaja@uw.edu>
2019-05-26 14:49:54 -0700

    added average time measurement for pyfhel

--- py/pyfheltest.py
+++ py/pyfheltest.py
@@ -1,20 +1,45 @@
 from Pyfhel import Pyfhel, PyPtxt, PyCtxt
+from datetime import datetime, timedelta
+
+numTrials = 5
+totalETime = timedelta(0)
+totalATime = timedelta(0)
+totalDTime = timedelta(0)
+
+print("=========================================================================")
+print("Pyfhel")
 
 # Creating context and KeyGen
 HE = Pyfhel()
 HE.contextGen(p=2048)
 HE.keyGen()
 
-# Encrypt integers
-pt1 = 5
-pt2 = 2
-ct1 = HE.encryptInt(pt1)
-ct2 = HE.encryptInt(pt2)
 
-# Operate on integers
-ctSum = ct1 + ct2
+for i in range(0, numTrials):
+	
+	
+	# Encrypt integers
+	startTime = datetime.now()
+	pt1 = 5
+	pt2 = 2
+	ct1 = HE.encryptInt(pt1)
+	ct2 = HE.encryptInt(pt2)
+	endTime = datetime.now()
+	totalETime = totalETime + (endTime - startTime)
 
-# Decrypt result
-res = HE.decryptInt(ctSum)
+	# Operate on integers
+	startTime = datetime.now()
+	ctSum = ct1 + ct2
+	endTime = datetime.now()
+	totalATime = totalATime + (endTime - startTime)
 
-print(res)
+	# Decrypt result
+	startTime = datetime.now()
+	res = HE.decryptInt(ctSum)
+	endTime = datetime.now()
+	totalDTime = totalDTime + (endTime - startTime)
+
+print("Avg encryption time: {0}".format(str(totalETime/(numTrials*2))))
+print("Avg decryption time: {0}".format(str(totalDTime/numTrials)))
+print("Avg addition time: {0}".format(str(totalETime/numTrials)))
+print("=========================================================================\n")
